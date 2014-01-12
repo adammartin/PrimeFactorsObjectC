e61615645a2fda13e9dd9074178ed16d6766d871
adammartin <ebondark.od@gmail.com>
2014-01-12 17:57:05 -0600

    Adding Prime Factors basic testing with hamcrest

---
+++ PrimeFactors/PrimeFactorsTests/PFTPrimeFactorsTest.m
@@ -0,0 +1,27 @@
+#define HC_SHORTHAND
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
+#import <XCTest/XCTest.h>
+
+@interface PFTPrimeFactorsTest : XCTestCase
+
+@end
+
+@implementation PFTPrimeFactorsTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+}
+
+@end
